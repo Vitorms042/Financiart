19712cb8cecbe7e2121c47b24a73fba160a77768
Vitor Manoel Silva <vitorms042@gmail.com>
2023-08-21 20:36:28 -0300

    Pasta Exercicio 1 - Ti2

--- ProjetoGitHub/src/SomarNumeros.java
+++ ProjetoGitHub/src/SomarNumeros.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-//Primeiro codigo Ti2
+//Primeiro codigo Ti2.
 public class SomarNumeros {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
